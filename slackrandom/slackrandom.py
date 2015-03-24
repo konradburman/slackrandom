2e02b49cd3121e51abd29570a69a3a7b060b9493
Wesley Stratton <stratton.wesley@gmail.com>
2015-03-24 00:20:27 -0400

    Random Int

--- slackrandom/slackrandom.py
+++ slackrandom/slackrandom.py
@@ -1,22 +1,25 @@
 import json
+import random
 from datetime import datetime
-from django.http import JsonResponse, Http404
+from django.http import HttpResponse, Http404
 from django.shortcuts import redirect
 from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
 def slackrandom(request):
-    responseData = {}
-    responseData["text"] = "DONE"
+    responseData = ""
+    requestToken = request.POST['token']
+    requestTeamId = request.POST['team_id']
+    requestTeamDomain = request.POST['team_domain']
+    requestChannelId = request.POST['channel_id']
+    requestChannelName = request.POST['channel_name']
+    requestUserId = request.POST['user_id']
+    requestUserName = request.POST['user_name']
+    requestCommand = request.POST['command']
+    requestText = request.POST['text']
+    systemRandom = random.SystemRandom()
 
-    print request.POST['token']
-    print request.POST['team_id']
-    print request.POST['team_domain']
-    print request.POST['channel_id']
-    print request.POST['channel_name']
-    print request.POST['user_id']
-    print request.POST['user_name']
-    print request.POST['command']
-    print request.POST['text']
+    responseData = systemRandom.randomint(0, 100)
 
-    return JsonResponse(responseData)
+
+    return HttpResponse(responseData)
