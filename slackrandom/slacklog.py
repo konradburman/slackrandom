b9b0a7ffed69659dbed1cd4238ebbc434ba99b25
Wesley Stratton <stratton.wesley@gmail.com>
2015-03-24 02:09:19 -0400

    Added Redis

--- slackrandom/slacklog.py
+++ slackrandom/slacklog.py
@@ -7,7 +7,7 @@ r = redis.Redis(unix_socket_path=settings.REDIS_SOCK)
 
 def info(source, message):
     log.info(source + "\t" + str(message))
-    bytes_generated(message)
+    bytes_generated(str(message))
 
 def bytes_generated(message):
     if not settings.REDIS_ENABLED: return
