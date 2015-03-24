571ef65c0e380bdd47b707935e2e113bb87cd01f
Wesley Stratton <stratton.wesley@gmail.com>
2015-03-24 00:29:48 -0400

    Random Int

--- slackrandom/slackrandom.py
+++ slackrandom/slackrandom.py
@@ -1,9 +1,13 @@
 import json
+import logging
 import random
+import uuid
 from datetime import datetime
 from django.http import HttpResponse, Http404
 from django.shortcuts import redirect
 from django.views.decorators.csrf import csrf_exempt
+
+logger = logging.getLogger(__name__)
 
 @csrf_exempt
 def slackrandom(request):
@@ -17,9 +21,15 @@ def slackrandom(request):
     requestUserName = request.POST['user_name']
     requestCommand = request.POST['command']
     requestText = request.POST['text']
-    systemRandom = random.SystemRandom()
 
-    responseData = systemRandom.randint(0, 100)
-
+    if requestText == "":
+        responseData = randomint(0, 100)
 
     return HttpResponse(responseData)
+
+def randomint(min, max):
+    integer = random.SystemRandom().randint(0, 100)
+    
+    logger.info(integer)
+
+    return integer
